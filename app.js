dbaec70892e2609b3575f7ca8b6a9189a5366efc
Riczuu <aleksander.informatyk@op.pl>
2020-09-10 17:27:06 +0200

    Added result page

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const app = express();
-
+const https = require('https');
 const bodyParser = require("body-parser");
+const { json } = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
 app.use(express.static("public"));
 
 app.get("/", (req, res) =>{
     res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) =>{
+    // const city = req.body.cityname;
+    // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=05af9f25cd15c7ddbf867e1152167c63&lang=pl&units=metric`
+    // https.get(url, (message) =>{
+    //     message.on("data", (data) =>{
+    //         const jsondata = JSON.parse(data);
+    //         res.render("weatherresults", {cityName: jsondata.name, 
+    //             temperature: jsondata.main.temp, 
+    //             feelliketempreature: jsondata.main.feels_like, 
+    //             pressure: jsondata.main.pressure, 
+    //             humidity: jsondata.main.humidity, 
+    //             windspeed: jsondata.wind.speed, 
+    //             winddirection: jsondata.wind.deg, 
+    //             visibility: jsondata.visibility, 
+    //             icon: jsondata.weather[0].icon, 
+    //             description: jsondata.weather[0].description})
+    //     });
+    // })
+    res.render("weatherresults");
+    console.log(city);
 });
 
 app.listen(3000, ()=>{

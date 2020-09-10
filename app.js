b8707d3b985e5062700d4a73924052281e5fab4e
Riczuu <aleksander.informatyk@op.pl>
2020-09-10 15:20:17 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs")
+app.use(express.static("public"));
+
+app.get("/", (req, res) =>{
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, ()=>{
+    console.log("Server started on port 3000");
+});

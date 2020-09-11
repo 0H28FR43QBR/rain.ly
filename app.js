0aa87fa96a1211632c47163516862f5e43ad19cc
Riczuu <aleksander.informatyk@op.pl>
2020-09-11 13:35:52 +0200

    Added functionality

--- app.js
+++ app.js
@@ -12,25 +12,23 @@ app.get("/", (req, res) =>{
 });
 
 app.post("/", (req, res) =>{
-    // const city = req.body.cityname;
-    // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=05af9f25cd15c7ddbf867e1152167c63&lang=pl&units=metric`
-    // https.get(url, (message) =>{
-    //     message.on("data", (data) =>{
-    //         const jsondata = JSON.parse(data);
-    //         res.render("weatherresults", {cityName: jsondata.name, 
-    //             temperature: jsondata.main.temp, 
-    //             feelliketempreature: jsondata.main.feels_like, 
-    //             pressure: jsondata.main.pressure, 
-    //             humidity: jsondata.main.humidity, 
-    //             windspeed: jsondata.wind.speed, 
-    //             winddirection: jsondata.wind.deg, 
-    //             visibility: jsondata.visibility, 
-    //             icon: jsondata.weather[0].icon, 
-    //             description: jsondata.weather[0].description})
-    //     });
-    // })
-    res.render("weatherresults");
-    console.log(city);
+    const city = req.body.cityname;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=05af9f25cd15c7ddbf867e1152167c63&lang=pl&units=metric`
+    https.get(url, (message) =>{
+        message.on("data", (data) =>{
+            const jsondata = JSON.parse(data);
+            const iconURL = `http://openweathermap.org/img/wn/${jsondata.weather[0].icon}@2x.png`;
+            res.render("weatherresults", {cityName: jsondata.name, 
+                temperature: jsondata.main.temp, 
+                pressure: jsondata.main.pressure, 
+                humidity: jsondata.main.humidity, 
+                windspeed: jsondata.wind.speed, 
+                winddirection: jsondata.wind.deg, 
+                visibility: jsondata.visibility, 
+                icon: iconURL, 
+                description: jsondata.weather[0].description})
+        });
+    })
 });
 
 app.listen(3000, ()=>{
